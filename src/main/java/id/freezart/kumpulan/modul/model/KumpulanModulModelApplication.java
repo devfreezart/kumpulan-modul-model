f5ee36510fbed63b1ad863917ecaad0437209134
devfreezart <devfreezart@gmail.com>
2018-06-18 10:50:52 -0700

    init project

---
+++ src/main/java/id/freezart/kumpulan/modul/model/KumpulanModulModelApplication.java
@@ -0,0 +1,12 @@
+package id.freezart.kumpulan.modul.model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KumpulanModulModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KumpulanModulModelApplication.class, args);
+	}
+}
